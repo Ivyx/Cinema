0777388cb107cb163beaf28dc7995a39e445dd7b
Ivyx <ivana.pietrabissa@outlook.com>
2017-11-02 16:21:42 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: ivyx
+ *
+ * Created on 2 novembre 2017, 16.20
+ */
+
+#include <cstdlib>
+
+#include "Cinema.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    string scelta;
+    Cinema TicketSeller = new Cinema();
+    
+    do
+    {
+        printMenu();
+        
+    }
+    
+    return 0;
+}
